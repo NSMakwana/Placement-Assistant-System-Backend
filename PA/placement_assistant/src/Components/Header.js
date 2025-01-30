a1f3f2d56bc6b4c7998f4e8bfc4280ec1f059eaa
NSMakwana <136146161+NSMakwana@users.noreply.github.com>
2025-01-30 15:21:16 +0530

    commit PA

---
+++ PA/placement_assistant/src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+
+<div className="header">
+  <h3 class ="admin-title">Admin Dashboard</h3>
+  <h3 class="header-title">Placement Assistant</h3>
+  <button className="logout-btn">Logout</button>
+</div>
+
+   
+  );
+};
+
+export default Header;
