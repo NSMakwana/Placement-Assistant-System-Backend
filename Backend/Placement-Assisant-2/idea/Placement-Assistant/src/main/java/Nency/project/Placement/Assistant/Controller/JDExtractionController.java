bd6f900b54ab6dd6684c4a383c9226b5a7b0f6cb
NSMakwana <136146161+NSMakwana@users.noreply.github.com>
2025-04-19 21:34:00 +0530

    commit

--- Backend/Placement-Assisant-2/idea/Placement-Assistant/src/main/java/Nency/project/Placement/Assistant/Controller/JDExtractionController.java
+++ Backend/Placement-Assisant-2/idea/Placement-Assistant/src/main/java/Nency/project/Placement/Assistant/Controller/JDExtractionController.java
@@ -4,13 +4,11 @@ import Nency.project.Placement.Assistant.model.Company;
 import Nency.project.Placement.Assistant.service.GeminiExtractionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public class JDExtractionController {
+    @CrossOrigin(origins = {"http://localhost:3000","https://placement-assistant-system.vercel.app"})
     @RestController
     @RequestMapping("/api/jd")
     public class jdextractioncontroller {
