9171f8cfd8916dfa6776a3e9ce308160202fc6fd
NSMakwana <136146161+NSMakwana@users.noreply.github.com>
2025-04-24 00:12:42 +0530

    commit

--- Backend/Placement-Assisant-2/idea/Placement-Assistant/src/main/java/Nency/project/Placement/Assistant/service/GeminiExtractionService.java
+++ Backend/Placement-Assisant-2/idea/Placement-Assistant/src/main/java/Nency/project/Placement/Assistant/service/GeminiExtractionService.java
@@ -105,8 +105,8 @@ public class GeminiExtractionService {
     @Value("${jwt.token.secret}") // Inject the Hugging Face API token
     private String API_TOKEN;
 
-//    private static final String ENDPOINT = "https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407";
-      private static final String ENDPOINT = "https://api-inference.huggingface.co/models/google/byt5-small";
+    private static final String ENDPOINT = "https://api-inference.huggingface.co/models/mistralai/Mistral-Nemo-Instruct-2407";
+//      private static final String ENDPOINT = "https://api-inference.huggingface.co/models/google/byt5-small";
     public Map<String, Object> extractCompanyDetailsFromJD(String jdText) throws IOException, InterruptedException {
         String prompt = buildPrompt(jdText);
         String rawResponse = sendToHuggingFace(prompt);
